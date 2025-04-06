62b2a23ec6e845296eaf61c3db71ca700c07e01d
Lance Alilio <ALB1095@dlsud.edu.ph>
2025-04-06 11:01:58 +0800

    Used project-reset to get an empty app folder

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { ScrollView, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <ScrollView>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </ScrollView>
+  );
+}
